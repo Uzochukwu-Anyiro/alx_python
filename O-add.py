f0da807bce5b344fc7fbc6152299d72d92a33d5e
Uzochukwu-Anyiro <uzochukwuanyiro@gmail.com>
2023-07-22 21:15:43 +0100

    my O-add file

---
+++ O-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add(a, b): 
+    return a + b
+
+# import the function from 0-add.py
+# from 0-add import add
+
+# perform the addition
+
+result = add(1, 2) 
+
+print("1 + 2 =", result) 
+
